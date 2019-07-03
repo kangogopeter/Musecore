bcdc551640e63821d1e2723af278b6dfc6dd3843
kangogopeter <peterkangogo501@gmail.com>
2019-07-03 12:23:44 +0300

    authorising emails

--- app/src/main/java/com/epicodus/muscore/ui/CreateAccountActivity.java
+++ app/src/main/java/com/epicodus/muscore/ui/CreateAccountActivity.java
@@ -1,9 +1,5 @@
 package com.epicodus.muscore.ui;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.VoiceInteractor;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,7 +9,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.epicodus.muscore.MainActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.epicodus.muscore.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
