90d0483a40fba7ba2c1caf91ea7cd67772386ced
kangogopeter <peterkangogo501@gmail.com>
2019-07-03 12:18:24 +0300

    functionality

--- app/src/main/java/com/epicodus/muscore/ui/CreateAccountActivity.java
+++ app/src/main/java/com/epicodus/muscore/ui/CreateAccountActivity.java
@@ -21,6 +21,9 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
@@ -84,7 +87,7 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 final FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null){
-                    Intent intent = new Intent(CreateAccountActivity.this, OptionActivity.class);
+                    Intent intent = new Intent(CreateAccountActivity.this, MenuActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
                     finish();
