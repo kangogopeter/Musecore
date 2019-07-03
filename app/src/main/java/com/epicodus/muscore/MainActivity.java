60cacbe3359f59f44ab99c09c527ba7e2b5b8af1
kangogopeter <peterkangogo501@gmail.com>
2019-07-03 13:40:55 +0300

    new additions

--- app/src/main/java/com/epicodus/muscore/MainActivity.java
+++ app/src/main/java/com/epicodus/muscore/MainActivity.java
@@ -2,16 +2,34 @@ package com.epicodus.muscore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.epicodus.muscore.ui.YoutubeActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @BindView(R.id.registerTextView)
+    Button mRegisterTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
     }
 
-    
+    @Override
+    public void onClick(View view){
+        if(view == mRegisterTextView){
+            Intent intent = new Intent(MainActivity.this, YoutubeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
 }
