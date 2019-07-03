2ec10e973dee2bd48da5bb09f805b3be7b84122c
kangogopeter <peterkangogo501@gmail.com>
2019-07-03 12:03:46 +0300

    functionality

--- app/src/main/java/com/epicodus/muscore/MenuActivity.java
+++ app/src/main/java/com/epicodus/muscore/MenuActivity.java
@@ -1,17 +1,14 @@
 package com.epicodus.muscore;
 
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity{
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_menu);
     }
-
 }
